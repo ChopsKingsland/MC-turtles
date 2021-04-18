5b614f8c2c454ecc3b0de1b88bdcbcf72fc834fd
Charlie Kingsland <charliekingsland@outlook.com>
2021-04-18 18:22:04 +0100

    initial commit

---
+++ doSomeMining.lua
@@ -0,0 +1,45 @@
+-- https://pastebin.com/bek5HnUC --
+
+turtle.refuel()
+
+local x = 10
+local y = 2
+local z = 10
+
+local i = 1
+local args = {...}
+
+--- GET X AND Z --
+
+if (args[1] ~= nil) then
+    x = args[1]
+    z = args[2]
+end
+
+for a = 1, z, 1 do
+    for b = 1, x - 1, 1 do
+        turtle.dig()
+        turtle.forward()
+
+    end
+
+    if (i == 1) then
+        turtle.turnRight()
+        turtle.dig()
+        turtle.forward()
+        turtle.turnRight()
+
+        i = 2
+    elseif (i == 2) then
+        turtle.turnLeft()
+        turtle.dig()
+        turtle.forward()
+        turtle.turnLeft()
+
+        i = 1
+    end
+
+    
+end
+
+print("Mining a " .. x .. "x" .. z .. " area")
