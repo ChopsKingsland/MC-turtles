8beb7f599ca53d6a24d96d355b5c8ccddc5f6b3b
Charlie Kingsland <charliekingsland@outlook.com>
2021-04-18 22:23:24 +0100

    mine

--- doSomeMining.lua
+++ doSomeMining.lua
@@ -9,41 +9,58 @@ local z = 10
 local i = 1
 local args = {...}
 
---- GET X AND Z --
+--- GET X, Y & Z --
 
 if (args[1] ~= nil) then
     x = args[1]
-    z = args[2]
+    y = args[2]
+    z = args[3]
 end
 
-for a = 1, z, 1 do
-    for b = 1, x - 1, 1 do
-        while(turtle.detect()) do
-            turtle.dig()
-            sleep(0.25)
+-- THE LOOP --
+
+for c = 1, y, 1 do
+    for a = 1, z, 1 do
+        for b = 1, x - 1, 1 do
+            while(turtle.detect()) do
+                turtle.dig()
+                sleep(0.25)
+            end
+
+            turtle.forward()
+
         end
+
+        if (a == z) then
+            
+        else
+            if (i == 1) then
+                turtle.turnRight()
+                turtle.dig()
+                turtle.forward()
+                turtle.turnRight()
         
-        turtle.forward()
-
+                i = 2
+            elseif (i == 2) then
+                turtle.turnLeft()
+                turtle.dig()
+                turtle.forward()
+                turtle.turnLeft()
+        
+                i = 1
+            end
+        end
     end
 
-    if (i == 1) then
-        turtle.turnRight()
-        turtle.dig()
-        turtle.forward()
-        turtle.turnRight()
-
-        i = 2
-    elseif (i == 2) then
-        turtle.turnLeft()
-        turtle.dig()
-        turtle.forward()
-        turtle.turnLeft()
-
-        i = 1
+    while(turtle.detectDown()) do
+        turtle.digDown()
+        sleep(0.25)
     end
 
-    
+    turtle.down()
+
+    -- change direction needed here --
+
 end
 
 print("Mining a " .. x .. "x" .. z .. " area, with a depth of " .. y)
